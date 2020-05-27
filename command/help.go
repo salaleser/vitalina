25c3b78eb4d3636fd9467dbe862fd14af1d5ff25
salaleser <salaleser@users.noreply.github.com>
2020-05-27 17:31:20 +0300

    Update

--- command/help.go
+++ command/help.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,27 +17,27 @@ func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if len(args) == 1 {
 		var builder strings.Builder
 
-		builder.WriteString("Список поддерживаемых комад (префикс `~`):\n")
+		builder.WriteString("Список поддерживаемых команд (префикс `~`):\n")
 		for title, description := range commands {
-			builder.WriteString("`" + title + "`")
-			builder.WriteString(" — ")
-			builder.WriteString(description)
-			builder.WriteByte('\n')
+			builder.WriteString(fmt.Sprintf(""+
+				"`%s` — %s\n",
+				title,
+				description,
+			))
 		}
+		builder.WriteString("\n")
 
 		text := "*Я переписываю бота на другой язык программирования. Я собираюсь " +
 			"восстановить большую часть старых команд в ближайшее время. Вопросы можно " +
 			"задать @salaleser#7570*\n\n" +
 			builder.String() +
-			"\nКроме того читает ID приложений App Store и Google Play (Steam будет " +
-			"добавлен) в тексте сообщений. А также текст, начинающийся с " +
-			"восклицательного знака воспринимает как команду для поиска в магазине " +
-			"приложений по ключевому запросу. Например: `!майнкрафт`." +
-			"\nВсе префиксы (алиасы для скрытых команд):" +
-			"\n`!` — search-apps (App Store, Google Play)" +
-			"\n`?` — force command (verbose mode)" +
-			"\n`.` — detect language (by reactions)" +
-			"\n`,` — detect language (by reply message)."
+			"(Usage) Примеры использования (попробуйте просто отправить такое сообщение):\n" +
+			"`143444`, `6014 CN`, `547702041 JP ja-jp`\n\n" +
+			"(Aliases) Все префиксы (алиасы для скрытых команд):\n" +
+			"`!` — search-apps (App Store, Google Play): `!майнкрафт`.\n" +
+			"`?` — force command (verbose mode): `?RU`, `?hu-hu`.\n" +
+			"`.` — detect language (by reactions): `.Աստղային ճանապարհ`\n" +
+			"`,` — detect language (by reply message) `,HeghluʼmeH QaQ jajvam`.\n"
 
 		util.SendInfo(s, m, text)
 	}
